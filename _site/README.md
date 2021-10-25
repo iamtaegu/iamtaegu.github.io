07dfdc82a0da5ad9b5de4cc3345e0acd2d6cda6c
iamtaegu <gmdals45@gmail.com>
2021-10-25 21:23:35 +0900

    20211025

--- _site/README.md
+++ _site/README.md
@@ -1,7 +1,7 @@
 # written by taegu
 bundle exec jekyll server (for adjust)
 # Personal Access Tokens
-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
+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
 
 # plainwhite
 
--- _site/스프링 핵심 원리/core/core/src/main/java/hello/core/member/Member.java
+++ _site/스프링 핵심 원리/core/core/src/main/java/hello/core/member/Member.java
@@ -1,12 +1,12 @@
 package hello.core.member;
 
+// Member Entity
 public class Member {
     private Long id;
     private String name;
     private Grade grade;
 
-    public Member(Long id, String name, Grade grade)
-    {
+    public Member(Long id, String name, Grade grade) {
         this.id = id;
         this.name = name;
         this.grade = grade;
@@ -16,20 +16,20 @@ public class Member {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public Grade getGrade() {
         return grade;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public void setGrade(Grade grade) {
